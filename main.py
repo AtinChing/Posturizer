d8d9d123f711e75e904e1c1cf73793bdfe843b81
Atin Kumar Singh <68199228+AtinChing@users.noreply.github.com>
2023-11-08 00:20:25 +0300

    Implemented/added incorrect posture detection
    Finally successfully added incorrect posture detection to the program, it now finally detects incorrect posture, despite a few errors here and there.
    This is the first version/commit which can actually detect incorrect human sitting posture, which was originally the entire point of Posturiser.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 import math
 from scipy import stats
+import tkinter.messagebox
+import time
+
 def view_image(i):
     cv2.imshow('view', i)
     cv2.waitKey(0)
@@ -40,74 +43,73 @@ for x, y, r in circles2[0]:
 
 cap = cv2.VideoCapture(0)
 
+def calculate_max_contrast_pixel(img_gray, x, y, h, top_values_to_consider=2, search_width = 20):
+    y = int(y)
+    x = int(x)
+    columns = img_gray[y:y+h, int(x-search_width/2):int(x+search_width/2)]
+    column_average = columns.mean(axis=1)
+    try:
+        gradient = np.gradient(column_average, 3)
+    except(Exception): return None
+    gradient = np.absolute(gradient) 
+    max_indicies = np.argpartition(gradient, -top_values_to_consider)[-top_values_to_consider:] 
+    max_values = gradient[max_indicies]
+    if(max_values.sum() < top_values_to_consider): return None 
+    weighted_indicies = (max_indicies * max_values)
+    weighted_average_index = weighted_indicies.sum() / max_values.sum()
+    try:
+        index = int(weighted_average_index)
+    except(Exception): return None 
+    index = y + index
+    return index
+
+def detect_shoulder(img_gray, face, direction, x_scale=0.75, y_scale=0.75):
+    x_face, y_face, w_face, h_face = face 
+
+    w = int(x_scale * w_face)
+    h = int(y_scale * h_face)
+    y = y_face + h_face * 5/4 
+    if(direction == "right"): x = x_face + w_face - w / 20 
+    if(direction == "left"): x = x_face - w + w/20 
+    rectangle = (x, y, w, h)
+
+    x_positions = []
+    y_positions = []
+    for delta_x in range(w):
+        this_x = x + delta_x
+        this_y = calculate_max_contrast_pixel(img_gray, this_x, y, h)
+        if(this_y is None): continue 
+        x_positions.append(this_x)
+        y_positions.append(this_y)
+
+    lines = []
+    for index in range(len(x_positions)):
+        lines.append((x_positions[index], y_positions[index]))
+
+    try:
+        slope, intercept, r_value, p_value, std_err = stats.linregress(x_positions,y_positions)
+        line_y0 = int(x_positions[0] * slope + intercept)
+        line_y1 = int(x_positions[-1] * slope + intercept)
+        line = [(x_positions[0], line_y0), (x_positions[-1], line_y1)]
+    except(ValueError):
+        return None
+
+    value = np.array([line[0][1], line[1][1]]).mean()
+
+    return line, lines, rectangle, value
+
 if not cap.isOpened():
     raise IOError("Cannot open webcam")
 
-while True:
+shoulder_y_values = []
+for i in range(5):
     ret, frame = cap.read()
     frame = cv2.resize(frame, None, fx=1.6, fy=1.6, interpolation=cv2.INTER_AREA)
+    cv2.imshow('Input', frame)
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml") 
-    upperbody_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_mcs_upperbody.xml") 
+
     grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    edges = cv2.Canny(i_gray, 10, 350)
-    i_blurred = cv2.GaussianBlur(i_gray, ksize = (29,1), sigmaX=0)
-
-    lines = cv2.HoughLinesP(edges, rho=1, theta=1 * np.pi/180.0, threshold=1, minLineLength=1, maxLineGap=20)
-    faces = face_cascade.detectMultiScale(frame, 1.03, 4)
-
-    # Temporary shoulder detection method taken from external project
-
-    def calculate_max_contrast_pixel(img_gray, x, y, h, top_values_to_consider=2, search_width = 20):
-        y = int(y)
-        x = int(x)
-        columns = img_gray[y:y+h, int(x-search_width/2):int(x+search_width/2)]
-        column_average = columns.mean(axis=1)
-        gradient = np.gradient(column_average, 3)
-        gradient = np.absolute(gradient) 
-        max_indicies = np.argpartition(gradient, -top_values_to_consider)[-top_values_to_consider:] 
-        max_values = gradient[max_indicies]
-        if(max_values.sum() < top_values_to_consider): return None 
-        weighted_indicies = (max_indicies * max_values)
-        weighted_average_index = weighted_indicies.sum() / max_values.sum()
-        index = int(weighted_average_index)
-        index = y + index
-        return index
-
-    def detect_shoulder(img_gray, face, direction, x_scale=0.75, y_scale=0.75):
-        x_face, y_face, w_face, h_face = face 
-
-        w = int(x_scale * w_face)
-        h = int(y_scale * h_face)
-        y = y_face + h_face * 5/4 
-        if(direction == "right"): x = x_face + w_face - w / 20 
-        if(direction == "left"): x = x_face - w + w/20 
-        rectangle = (x, y, w, h)
-
-        x_positions = []
-        y_positions = []
-        for delta_x in range(w):
-            this_x = x + delta_x
-            this_y = calculate_max_contrast_pixel(img_gray, this_x, y, h)
-            if(this_y is None): continue 
-            x_positions.append(this_x)
-            y_positions.append(this_y)
-
-        lines = []
-        for index in range(len(x_positions)):
-            lines.append((x_positions[index], y_positions[index]))
-
-        try:
-            slope, intercept, r_value, p_value, std_err = stats.linregress(x_positions,y_positions)
-            line_y0 = int(x_positions[0] * slope + intercept)
-            line_y1 = int(x_positions[-1] * slope + intercept)
-            line = [(x_positions[0], line_y0), (x_positions[-1], line_y1)]
-        except(ValueError):
-            return None
-
-        value = np.array([line[0][1], line[1][1]]).mean()
-
-        return line, lines, rectangle, value
-    # Temporary borrowed code ends here
+    faces = face_cascade.detectMultiScale(frame, 1.05, 16)
     left_shoulder = detect_shoulder(grayscale, faces[0], "left")
     right_shoulder = detect_shoulder(grayscale, faces[0], "right")
     if left_shoulder == None or right_shoulder == None: continue
@@ -115,22 +117,49 @@ while True:
     right_shoulder_rectangle = right_shoulder[2]
     left_shoulder_x, left_shoulder_y = left_shoulder[2][0], left_shoulder[2][1] 
     right_shoulder_x, right_shoulder_y = right_shoulder[2][0], right_shoulder[2][1] 
+    shoulder_y_values.append((left_shoulder_y + right_shoulder_y)/2)
 
-    reference_line_angle = math.atan2(right_shoulder_y - left_shoulder_y, right_shoulder_x - left_shoulder_x)
+average_correct_y_height = 0 
+for value in shoulder_y_values: average_correct_y_height += value
+average_correct_y_height /= 5 
 
-    reference_line_angle_degrees = math.degrees(reference_line_angle)
+while True:
 
-    slouch_threshold = 10  
-    print(reference_line_angle_degrees)
-    if abs(reference_line_angle_degrees) > slouch_threshold:
-        print("Slouching shoulders detected")
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, None, fx=1.6, fy=1.6, interpolation=cv2.INTER_AREA)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml") 
+
+    grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(i_gray, 10, 350)
+    i_blurred = cv2.GaussianBlur(i_gray, ksize = (29,1), sigmaX=0)
+
+    lines = cv2.HoughLinesP(edges, rho=1, theta=1 * np.pi/180.0, threshold=1, minLineLength=1, maxLineGap=20)
+    faces = face_cascade.detectMultiScale(frame, 1.03, 4, minSize=(100, 100))
+
+    try:
+        left_shoulder = detect_shoulder(grayscale, faces[0], "left")
+        right_shoulder = detect_shoulder(grayscale, faces[0], "right")
+        if left_shoulder == None or right_shoulder == None: continue
+        left_shoulder_rectangle = left_shoulder[2]
+        right_shoulder_rectangle = right_shoulder[2]
+        left_shoulder_x, left_shoulder_y = left_shoulder[2][0], left_shoulder[2][1] 
+        right_shoulder_x, right_shoulder_y = right_shoulder[2][0], right_shoulder[2][1] 
+        average_shoulder_height = (left_shoulder_y + right_shoulder_y)/2
+        shoulder_slouch_diff = average_correct_y_height - average_shoulder_height
+    except(Exception):
+        continue
+
+    slouch_threshold = 15  
+
+    if abs(shoulder_slouch_diff) > slouch_threshold: 
+        print("Bad posture")
+        print(f'correct level is {average_correct_y_height} \n current level is {average_shoulder_height} \n difference is {shoulder_slouch_diff}')
     else:
         print("Good posture")
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
 
-    print(left_shoulder_rectangle)
     cv2.rectangle(frame, (int(left_shoulder_rectangle[0]), int(left_shoulder_rectangle[1])), (int(left_shoulder_rectangle[0]) + left_shoulder_rectangle[2], int(left_shoulder_rectangle[1]) + left_shoulder_rectangle[3]), (0, 255, 0), 5)
     cv2.rectangle(frame, (int(right_shoulder_rectangle[0]), int(right_shoulder_rectangle[1])), (int(right_shoulder_rectangle[0]) + right_shoulder_rectangle[2], int(right_shoulder_rectangle[1]) + right_shoulder_rectangle[3]), (0, 255, 0), 5)
 
